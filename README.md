35495e86327fee3c2978a2a04cb3d5db253bf0fb
LakshmanKishore <lakshmankishore17@gmail.com>
2022-10-20 15:48:52 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Shell Scripts
+
+This repository includes scripts of command prompt and powershell.
---
+++ init.cmd
@@ -0,0 +1,13 @@
+@echo off 
+
+echo Hello Lakshman!
+echo It is: %date%
+
+doskey jl=jupyter lab $*
+doskey clear=cls $*
+doskey gs=git status $*
+doskey gaa=git add . $*
+doskey ca=conda activate "$*"
+doskey cdec=conda deactivate 
+doskey cel=conda env list
+doskey cl=conda list
